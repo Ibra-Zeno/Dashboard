b6c529c87c1e0aa079640647cf8b6bdc0211c3dd
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-30 17:56:55 +0000

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface pageProps {
+  // props
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <p>Customers Page</p>;
+};
+
+export default page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface pageProps {
+  // props
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <p>Dashboard Page</p>;
+};
+
+export default page;
