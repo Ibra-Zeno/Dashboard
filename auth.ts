b121baadf116ac6c9530ba40c70fcb2586ae23e1
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-31 21:15:25 +0000

    validation & bcrypt

---
+++ auth.ts
@@ -0,0 +1,54 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "./auth.config";
+import { z } from "zod";
+import type { User } from "@/app/lib/definitions";
+import bcrypt from "bcrypt";
+require("dotenv").config();
+import postgres from "postgres";
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
+  idle_timeout: 5000,
+});
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql`SELECT * from USERS where email=${email}`;
+    console.log(user[0]);
+    return user[0] as User;
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordMatches = await bcrypt.compare(password, user.password);
+          if (passwordMatches) return user;
+        }
+
+        console.log("Invalid credentials");
+        return null;
+      },
+    }),
+  ],
+});
